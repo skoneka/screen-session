3cfe1c00bccda22ef6462c8d1a9c7d0c68404bf9
Artur Skonecki <adaszgred2@gmail.com>
2010-09-27 15:32:16 +0200

    screen-in-dir python rewrite

---
+++ ScreenSession/screen-in-dir.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os,sys
+import GNUScreen as sc
+
+session="%s"%sys.argv[1]
+session_arg="-S %s"%session
+
+cwin = sc.get_current_window(session)
+windows_old = sc.parse_windows(sc.get_windows(session))[0]
+print windows_old
+
+f = os.popen('screen %s -Q @tty'%session_arg)
+ctty=f.readline()
+f.close()
+
+f = os.popen('lsof -F p %s | cut -c2- | tail -1' % ctty)
+thedir = f.readline().strip()
+f.close()
+
+thedir = '/proc/%s/cwd' % thedir
+print thedir
+
+os.chdir(thedir)
+
+#command='screen %s -X screen' % (session_arg)
+command='screen'
+
+for arg in sys.argv[2:]:
+    command+=' '+arg
+print command
+os.system(command)
+
+windows_new=sc.parse_windows(sc.get_windows(session))[0]
+print windows_new
+windows_diff=sc.find_new_windows(windows_old, windows_new)
+target=windows_diff[0]
+print '%s to %s'%(target,cwin)
+sc.move(int(target),int(cwin)+1,True,session)
+
