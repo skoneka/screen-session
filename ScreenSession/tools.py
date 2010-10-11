57869524de71b9be49c662dd4666770d06af68d8
Artur Skonecki <adaszgred2@gmail.com>
2010-10-11 21:33:40 +0200

    add sort tool

--- ScreenSession/tools.py
+++ ScreenSession/tools.py
@@ -33,32 +33,32 @@ def sort(session,min,max,key=None):
     cgroup=None
     for win,type,title in sc.gen_all_windows(min,max,session):
         iwin=int(win)
-        group=ss.getgroup(win)
-        if cgroup!=group:
-            cgroup=group
-            wins=groups[cgroup]=[]
-        groups[cgroup].append((group,iwin,type,title))
+        group=ss.get_group(win)
 
-    win_biggest=wins[len(wins)-1][1]
+        lastval=(group,iwin,type,title)
+        try:
+            groups[group].append(lastval)
+        except:
+            groups[group]=[lastval]
+            
+    win_biggest=lastval[1]
     for i in range(0,win_biggest+1):
         wins_trans[i]=i
 
-    wins.sort(key=lambda wins:wins[3].lower())
-
-    wins.sort(key=lambda wins:wins[0])
-    nwins=[]
-
-
-
     i=0
-    for group,win,type,title in wins:
-        if wins_trans[win]!=i:
-            #print("win %d(%d)(%s) as %d"%(wins_trans[win],win,group,i))
-            ss.number(str(i),str(wins_trans[win]))
-            tmp=wins_trans[win]
-            wins_trans[win]=wins_trans[i]
-            wins_trans[i]=tmp
-        i+=1
+    for group,props in groups.items():
+        props.sort(key=lambda wins:wins[3].lower())
+        #print( str(len(props))+' : '+group + ' : ' + str(props))
+        for group,win,type,title in props:
+            if wins_trans[win]!=i:
+                #print("win %d(%d)(%s) as %d"%(wins_trans[win],win,group,i))
+                ss.number(str(i),str(wins_trans[win]))
+                tmp=wins_trans[win]
+                wins_trans[win]=wins_trans[i]
+                wins_trans[i]=tmp
+            i+=1
+    return
+
 
 def kill_zombie(session,min,max):
     ss=ScreenSaver(session,'/dev/null','/dev/null')
