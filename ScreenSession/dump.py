baff38c4e4c1592103a8fde2754be60fdb3012b6
Artur Skonecki <adaszgred2@gmail.com>
2010-11-01 21:54:28 +0100

    add dump.py to source control

---
+++ ScreenSession/dump.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/env python
+
+import sys
+import GNUScreen as sc
+from ScreenSaver import ScreenSaver
+import tools
+
+
+
+if __name__=='__main__':
+    session=sys.argv[1]
+    ss=ScreenSaver(session)
+    try:
+        maxwin=int(sys.argv[2])
+    except:
+        maxwin=ss.maxwin()
+        minwin=0
+    else:
+        try:
+            minwin=int(sys.argv[3])
+        except:
+            minwin=0
+    tools.dump(ss,minwin,maxwin)
+
