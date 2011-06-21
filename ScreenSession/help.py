938b91a770e7a95088baed80835a2159557a5d0d
Artur Skonecki <adaszgred2@gmail.com>
2011-06-21 20:59:00 +0200

    merge HOWTO into help.py

--- ScreenSession/help.py
+++ ScreenSession/help.py
@@ -46,7 +46,9 @@ Report bugs to http://github.com/skoneka/screen-session/issues\
 "
 
 help_regions="Usage: screen-session regions [options]\n\
-       scs r [options]\n\n\
+       scs r [options]\n\
+       :bind X at 0 exec scs regions\n\
+\n\
 Display a number in each region.\n\
 Allows selecting, swapping and rotating. Inspired by tmux display-panes.\n\
 \n\
@@ -63,7 +65,8 @@ rotate right and select -> number and <R>\
 "
 
 help_kill="Usage: screen-session kill [options] [signal=TERM] [window=current]\n\
-       scs K [options] [signal=TERM] [window=current]\n\n\
+       scs K [options] [signal=TERM] [window=current]\n\
+\n\
 Kill last process started in a window.\n\
 Useful for closing random emacs/vim instances and hung up ssh clients.\n\
 For a list of signal names run: $ pydoc signal\n\
@@ -71,12 +74,14 @@ WARNING: sending SIGKILL to the current window may crash Screen\
 "
 
 help_kill_zombie="Usage: screen-session kill-zombie [options] [groupids]\n\
-       scs kz [options] [groupids]\n\n\
+       scs kz [options] [groupids]\n\
+\n\
 Kill all zombie windows in session. Optionally specify target groups.\
 "
 
 help_kill_group="Usage: screen-session kill-group [options] [groupNum0] [groupNum..]\n\
-       scs kg [options] [groupNum0] [groupNum..]\n\n\
+       scs kg [options] [groupNum0] [groupNum..]\n\
+\n\
 Recursively kill groups and windows inside.\n\
 Accepts group window numbers as arguments.\n\
 If the first argument is \"current\" kill the current group.\n\
@@ -84,31 +89,42 @@ If the first argument is \"all\" kill all groups in the session.\n\
 Take extra care with this command.\
 "
 
-help_new_window="Usage: screen-session new-window [options] [program]\n\
+help_new_window='Usage: screen-session new-window [options] [program]\n\
        scs new [options] [program]\n\
-       scs dir [options] [program]\n\n\
+       scs dir [options] [program]\n\
+       :bind c eval "colon" "stuff \\"at 0 exec scs new-window \\""\n\
+\n\
 Start a new Screen window in the same working directory\n\
 on the position next to the current window.\
-"
+'
 
 help_dump="Usage: screen-session dump [options] [window_ids]\n\
-       scs d [options] [window_ids]\n\n\
+       scs d [options] [window_ids]\n\
+\n\
 Print informations about windows in session (most recently used first).\n\
 A dot \".\" selects current window, 2 dots \"..\"  select current group.\n\
+\n\
+Options:\n\
 -P \t- do not show pid data\n\
 -r \t- reverse the output\n\
 -s \t- sort by window number\
 "
 
 help_find_pid="Usage: screen-session find-pid [options] [PIDs]\n\
-       scs fp [options] [PIDs]\n\n\
-Example: screen-session find-pid $(pgrep vim)\n\n\
-Find PIDs in windows.\
+       scs fp [options] [PIDs]\n\
+\n\
+Find PIDs in windows.\n\
+Example: screen-session find-pid $(pgrep vim)\
 "
 
 help_find_file="Usage: screen-session find-file [options] [files]\n\
-       scs ff [options] [files]\n\n\
-Find open files in windows. Requires lsof.\
+       scs ff [options] [files]\n\
+\n\
+Find open files in windows. Requires lsof.\n\
+\n\
+Example:\n\
+tail -f /var/log/dmesg\n\
+scs find-file /var/log/dmesg\
 "
 
 help_grab="Grab a process and attach to the current tty.\n\
@@ -120,15 +136,18 @@ and on the previous shell type:\n\
 $ disown\n\
 It works more reliably if commands from the script are typed manually."
 
-help_group="Usage: screen-session group [options] [GROUP] [windows]\n\
-       scs g [options] [GROUP] [windows]\n\n\
+help_group='Usage: screen-session group [options] [GROUP] [windows]\n\
+       scs g [options] [GROUP] [windows]\n\
+       :bind G eval "colon" "stuff \\"at 0 exec scs group \\""\n\
+\n\
 Move windows to a group.\n\
 If no windows given, move the current window.\
-"
+'
 
 #help_manager="Usage: screen-session manager [account@host]\n\
 help_manager="Usage: screen-session manager\n\
-       scs m\n\n\
+       scs m\n\
+\n\
 Sessions manager for GNU Screen with preview in a split window.\n\
 Reads custom Screen configuration from $HOME/.screenrc_MANAGER.\n\
 Requires python 2.5+\n\
@@ -155,32 +174,38 @@ l[ayout]  - toggle layout\n\
 kill K    - kill selected session\n\
 "
 
-help_manager_remote="Usage: screen-session manager-remote\n\n\
+help_manager_remote="Usage: screen-session manager-remote\n\
+\n\
 Sessions manager for GNU Screen with preview in a split window and support for multiple hosts.\n\
 Requires python 2.5+\
 "
 
 help_nest="Usage: screen-session nest-layout [options] [TARGET_LAYOUT]\n\
-       scs nl [options] [TARGET_LAYOUT]\n\n\
+       scs nl [options] [TARGET_LAYOUT]\n\
+\n\
 Nest a layout in the current region.\
 "
 
-help_renumber="Usage: screen-session renumber [options]\n\n\
+help_renumber="Usage: screen-session renumber [options]\n\
+\n\
 Renumber windows to fill the gaps.\
 "
 
-help_sort="Usage: screen-session sort [options]\n\n\
+help_sort="Usage: screen-session sort [options]\n\
+\n\
 Sort windows by titles.\
 "
 
 help_subwindows="Usage: screen-session subwindows [groupids or titles]\n\
-       scs sw [groupids or titles]\n\n\
+       scs sw [groupids or titles]\n\
+\n\
 Print windows contained in groups.\n\
 A dot \".\" selects current window, 2 dots \"..\"  select current group.\
 "
 
 help_name="Usage: screen-session name [options] [new_sessionname]\n\
-       scs n [options] [new_sessionname]\n\n\
+       scs n [options] [new_sessionname]\n\
+\n\
 Get or set sessionname.\
 "
 
@@ -188,9 +213,11 @@ help_saver_modes='GNU Screen session saver.\n\
 Usage: screen-session [save|load|ls] [-S sessionname] [options] [savefile]'
 
 
-help_saver_other="Usage: screen-session other [options] \n\n\
+help_saver_other="Usage: screen-session other [options] \n\
+ \n\
 Garden of Eden Creation Kit for GNU Screen.\n\
 Auxiliary mode, used mainly by screen-session-primer.\n\
+\n\
 Options:\n\
 --pack <target>\n\
   \tarchive unpacked savefile ( which must be accessible from --dir )\n\
@@ -202,9 +229,11 @@ Options:\n\
   \tdirectory holding saved sessions (default: $HOME/.screen-sessions)\
 "
 
-help_saver_ls="Usage: screen-session save [-S sessionname] [options] [savefile_filter]\n\n\
+help_saver_ls="Usage: screen-session save [-S sessionname] [options] [savefile_filter]\n\
+ \n\
 Garden of Eden Creation Kit for GNU Screen.\n\
 List saved sesssions.\n\
+\n\
 Options:\n\
 -l --log  <file>\n\
   \toutput to a file instead of stdout\n\
@@ -212,9 +241,12 @@ Options:\n\
   \tdirectory holding saved sessions (default: $HOME/.screen-sessions)\
 "
 
-help_saver_save="Usage: screen-session save [-S sessionname] [options] [target_savefile]\n\n\
+help_saver_save="Usage: screen-session save [-S sessionname] [options] [target_savefile]\n\
+       :bind S at 0 exec screen -mdc /dev/null screen-session save -H SECURE -f -S $PID.$STY\n\
+\n\
 Garden of Eden Creation Kit for GNU Screen.\n\
 Save GNU Screen and VIM sessions to a file.\n\
+\n\
 Options:\n\
 -f --force\n\
   \tforce saving even if savefile with the same name already exists\n\
@@ -233,13 +265,23 @@ Options:\n\
   \toutput to a file instead of stdout\n\
 -d --dir  <directory>\n\
   \tdirectory holding saved sessions (default: $HOME/.screen-sessions)\n\
-Example [ save Screen named SESSIONNAME as mysavedsession ]:\n\
-$ screen-session save -S SESSIONNAME mysavedsession\
+\n\
+Examples:\n\
+#1# save Screen named SESSIONNAME as mysavedsession\n\
+screen-session save -S SESSIONNAME mysavedsession\n\
+#2# save the current session, force overwrite of old savefiles\n\
+scs save --force\n\
+#3# save the current session without layouts\n\
+scs save --no-layout\n\
+#4# run session saver after 3 minutes of inactivity.\n\
+:idle 180 at 0 exec scs save --force --log /dev/null\
 "
 
-help_saver_load="Usage: screen-session load [-S sessionname] [options] [source_savefile]\n\n\
+help_saver_load="Usage: screen-session load [-S sessionname] [options] [source_savefile]\n\
+ \n\
 Garden of Eden Creation Kit for GNU Screen.\n\
 Load saved session from a file.\n\
+\n\
 Options:\n\
 -x --exact\n\
   \tload session with the same window numbers, move existing windows\n\
@@ -260,8 +302,18 @@ Options:\n\
   \toutput to a file instead of stdout\n\
 -d --dir  <directory>\n\
   \tdirectory holding saved sessions (default: $HOME/.screen-sessions)\n\
-Example [ restore mysavedsession inside Screen named SESSIONNAME ]:\n\
-$ screen-session load -S SESSIONNAME --exact mysavedsession\
+\n\
+Examples:\n\
+#1# restore mysavedsession inside Screen named SESSIONNAME\n\
+screen-session load -S SESSIONNAME --exact mysavedsession\n\
+#2# load the last saved session inside the current Screen session\n\
+scs load\n\
+#3# load the last saved session with exactly the same window numbers\n\
+scs load --exact\n\
+#4# load the last saved session inside the current session without layouts\n\
+scs load --no-layout\n\
+#5# load the last saved session into a new Screen\n\
+screen -m scs load --exact-kill\
 "
 
 def run(argv):
@@ -269,7 +321,7 @@ def run(argv):
         print(help_regions)
         print(help_kill_zombie)
         print(help_kill_cgroup)
-        print(help_dir)
+        print(help_new_window)
         print(help_dump)
         print(help_grab)
         print(help_group)
