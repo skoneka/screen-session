9704dc8e02f8efa33a0848a3848a159d1f868d40
Artur Skonecki <admin@adb.cba.pl>
2011-07-20 18:38:42 +0200

    update authors

--- ScreenSession/screen-session-primer.c
+++ ScreenSession/screen-session-primer.c
@@ -1,7 +1,7 @@
 /*    
  *    Copyright (C) 2010-2011 Artur Skonecki
  *
- *    Authors: Artur Skonecki <admin [>at<] adb.cba.pl>
+ *    Authors: Artur Skonecki http://github.com/skoneka
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
--- ScreenSession/tools.py
+++ ScreenSession/tools.py
@@ -3,8 +3,8 @@
 #
 #    Copyright (C) 2010-2011 Artur Skonecki
 #
-#    Authors: Artur Skonecki <admin [>at<] adb.cba.pl>
-#             Brendon Crawford http://github.com/last/
+#    Authors: Artur Skonecki http://github.com/skoneka
+#             Brendon Crawford https://github.com/brendoncrawford
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
