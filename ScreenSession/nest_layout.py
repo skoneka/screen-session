a5cb380a7841f64c42a11e6f861b710656c75e0c
Artur Skonecki <admin@adb.cba.pl>
2011-07-27 17:46:10 +0200

    nest-layout is now mostly fixed and really able to nest layouts in regions

--- ScreenSession/nest_layout.py
+++ ScreenSession/nest_layout.py
@@ -22,81 +22,82 @@
 import os
 import sys
 import GNUScreen as sc
-from util import tmpdir,remove
+from util import tmpdir, tmpdir_source, remove
 from ScreenSaver import ScreenSaver
 
 
-def main():
-    logfile = "___log-nest-layout"
-    dumpfile = "___scs-nest-layout-dump-%d" % os.getpid()
-    if not os.path.exists(tmpdir):
-        os.makedirs(tmpdir)
-    logfile = os.path.join(tmpdir, logfile)
-    dumpfile = os.path.join(tmpdir, dumpfile)
-    sys.stdout = open(logfile, 'w')
-    sys.stderr = sys.stdout
-    session = (sys.argv)[1]
-    tlayout = (sys.argv)[2]
+def nest_layout(session, src_layuot, dst_layout):
+    src_dumpfile = os.path.join(tmpdir_source, 'nest_layout-dump-%d' % os.getpid())
+
+    if not os.path.exists(tmpdir_source):
+        os.makedirs(tmpdir_source)
 
     scs = ScreenSaver(session)
-    (homelayout, homelayoutname) = scs.get_layout_number()
 
-    regions_file_org = regions_file = sc.dumpscreen_layout(scs.pid)
-    regions_org = regions = sc.get_regions(regions_file)
+    print('layouts src: %s dst: %s' % (src_layout, dst_layout))
 
-    focusminsize = "%s %s" % (regions.focusminsize_x, regions.focusminsize_y)
-    regions_c = len(regions.regions)
-    foff = regions.focus_offset
-    rsize = tuple([int((regions.regions)[foff][1]), int((regions.regions)[foff][2])])
-    hsize = (int(regions.term_size_x), int(regions.term_size_y))
-    print ("homelayout: %s" % homelayout)
+    regions_file_dst = regions_file = sc.dumpscreen_layout(scs.pid)
+    regions_dst = sc.get_regions(regions_file)
+
+    dst_focusminsize = "%s %s" % (regions_dst.focusminsize_x, regions_dst.focusminsize_y)
+    dst_rsize = (int(regions_dst.regions[regions_dst.focus_offset][1]),
+                int(regions_dst.regions[regions_dst.focus_offset][2]))
+    dst_term_size = (int(regions_dst.term_size_x),
+                    int(regions_dst.term_size_y))
     scs.focusminsize('0 0')
-    rsize = (rsize[0], rsize[1] + 2)  # +1 for caption +1 for hardstatus
+    scs.layout('select %s' % src_layout, False)
 
-    scs.layout('select %s' % tlayout, False)
-    print ("tlayout : %s" % scs.get_layout_number()[0])
-    scs.layout('dump %s' % dumpfile, False)
+    scs.layout('dump %s' % src_dumpfile, False)
 
-    regions_file = sc.dumpscreen_layout(scs.pid)
-    regions = sc.get_regions(regions_file)
+    regions_file_src = sc.dumpscreen_layout(scs.pid)
+    regions_src = sc.get_regions(regions_file_src)
 
-    tfocusminsize = "%s %s" % (regions.focusminsize_x, regions.focusminsize_y)
-    regions_c = len(regions.regions)
-    tfoff = regions.focus_offset
-    hsize = (int(regions.term_size_x), int(regions.term_size_y))
+    src_term_size = (int(regions_src.term_size_x), int(regions_src.term_size_y))
 
-    if rsize[0] == hsize[0] and rsize[1] + 2 == hsize[1]:
-        rsize = hsize
-    else:
-        rsize = (rsize[0], rsize[1] + 1)  # +1 for caption +1 for hardstatus
-    rsize = hsize
+    print ('dst_rsize: %s' % str(dst_rsize))
+    print ('src_term_size: %s' % str(src_term_size))
 
-    print ('rsize %s' % str(rsize))
-    print ('dinfo: %s' % str(hsize))
+    scs.layout('select %s' % dst_layout, False)
+    scs.source(src_dumpfile)
+    
+    regions_new = sc.Regions()
+    regions_new.focus_offset =  regions_dst.focus_offset + regions_src.focus_offset
+    regions_new.term_size_x = regions_dst.term_size_x
+    regions_new.term_size_y = regions_dst.term_size_y
+    regions_new.focusminsize_x = regions_dst.focusminsize_x
+    regions_new.focusminsize_y = regions_dst.focusminsize_y
+    regions_new.regions = regions_dst.regions[:regions_dst.focus_offset]
 
-    scs.layout('select %s' % homelayout, False)
-    scs.source(dumpfile)
+    for (window, sizex, sizey) in regions_src.regions:
+        print('SRC REGION' + str((window,sizex,sizey)))
+        x = (int(sizex) * dst_rsize[0]) / src_term_size[0]
+        y = (int(sizey) * dst_rsize[1]) / src_term_size[1]
+        print( '%s * %d / %d = %d' % (sizex, dst_rsize[0], src_term_size[0], x))
+        print( '%s * %d / %d = %d' % (sizey, dst_rsize[1], src_term_size[0], y))
+        regions_new.regions.append((window, str(x), str(y)))
 
-    #sc.load_regions(session, regions_org, None, hsize[0], hsize[1])
-    scs.select_region(foff)
-    for r in regions.regions:
-        if r[0][0] == '-':
-            scs.select('-')
-        else:
-            scs.select(r[0])
-        x = ((int(r[1]) + 1) * rsize[0]) / hsize[0]
-        y = (int(r[2]) * rsize[1]) / hsize[1]
-        scs.resize('-h %d' % x)
-        scs.resize('-v %d' % y)
-        print ('%s size %d %d' % (r[0], x, y))
-        scs.focus()
-    scs.select_region(foff + tfoff)
-    scs.focusminsize(focusminsize)
+    regions_new.regions = regions_new.regions + regions_dst.regions[regions_dst.focus_offset+1:]
+    
+    print('destination regions: '+ str(regions_dst.regions))
+    print('source regions: ' + str(regions_src.regions))
+    print('new regions: ' + str(regions_new.regions))
 
-    remove(dumpfile)
-    remove(regions_file)
-    remove(regions_file_org)
+    sc.load_regions(session, regions_new, None, dst_term_size[0], dst_term_size[1])
+
+    remove(src_dumpfile)
+    remove(regions_file_dst)
+    remove(regions_file_src)
     sc.cleanup()
 
 if __name__ == '__main__':
-    main()
+    logfile = os.path.join(tmpdir, '___log_nest-layout')
+    sys.stdout = open(logfile, 'w')
+    sys.stderr = sys.stdout
+
+    session = (sys.argv)[1]
+    src_layout = (sys.argv)[2]
+
+    scs = ScreenSaver(session)
+    dst_layout = scs.get_layout_number()[0]
+
+    nest_layout(session, src_layout, dst_layout)
